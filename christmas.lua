617181ab97a14c12ee97f0f987c89336caea4c41
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2025-12-15 14:17:43 +0100

    Update christmas.lua

--- christmas.lua
+++ christmas.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Steal a Pet 😹", HidePremium = false, SaveConfig = true, ConfigFolder = "Steal a Pet 😹"})
+local Window = OrionLib:MakeWindow({Name = "Murder Mystery 2", HidePremium = false, SaveConfig = true, ConfigFolder = "Murder Mystery 2"})
 
 local Tab = Window:MakeTab({
 	Name = "Farm",
@@ -7,19 +7,54 @@ local Tab = Window:MakeTab({
 	PremiumOnly = false
 })
 
+
+
+
+
+--------------------------------MISC-----------------------------
+local Tab = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
 Tab:AddButton({
-	Name = "Speed max ⚡",
+	Name = "Add Speed",
 	Callback = function()
     local RunService = game:GetService("RunService")
 local player = game.Players.LocalPlayer
 
 RunService.RenderStepped:Connect(function()
     if player.Character and player.Character:FindFirstChild("Humanoid") then
-        player.Character.Humanoid.WalkSpeed = 50
+        player.Character.Humanoid.WalkSpeed = 30
     end
 end)
   	end    
 })
+
+Tab:AddToggle({
+	Name = "Speed",
+	Default = false,
+	Callback = function(Value)
+		if Value == true then
+			print("Тумблер ВКЛЮЧЕН")
+			local player = game.Players.LocalPlayer
+
+RunService.RenderStepped:Connect(function()
+    if player.Character and player.Character:FindFirstChild("Humanoid") then
+        player.Character.Humanoid.WalkSpeed = 30
+		else
+			print("Тумблер ВЫКЛЮЧЕН")
+			local player = game.Players.LocalPlayer
+
+RunService.RenderStepped:Connect(function()
+    if player.Character and player.Character:FindFirstChild("Humanoid") then
+        player.Character.Humanoid.WalkSpeed = 16
+		end
+	end
+})
+
+
 Tab:AddButton({
 	Name = "Infinite Yield",
 	Callback = function()
@@ -27,5 +62,6 @@ Tab:AddButton({
   	end    
 })
 OrionLib:Init()
+
 
 
