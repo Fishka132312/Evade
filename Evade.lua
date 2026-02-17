3c5596b2b939fd26d539c6d9b5f0fbed6e7138bd
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-02-17 19:31:29 +0100

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -218,7 +218,7 @@ sendMessage(message)
 })
 
 Tab:AddButton({
-	Name = "XP FARM",
+	Name = "Shutdown Game if dev join",
 	Callback = function()
 			local Players = game:GetService("Players")
 local TextChatService = game:GetService("TextChatService")
@@ -228,6 +228,7 @@ local rewardsGui = player:WaitForChild("PlayerGui"):WaitForChild("Global"):WaitF
 
 _G.AutoFarmActive = true
 
+-- Функция отправки сообщений
 local function sendMessage(msg)
     if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
         local channel = TextChatService.TextChannels:FindFirstChild("RBXGeneral")
@@ -240,10 +241,11 @@ local function sendMessage(msg)
     end
 end
 
+-- Основная логика после появления Rewards
 local function onGameEnd()
     print("Игра завершена (Rewards найден). Выполняю цепочку команд...")
     
-    rewardsGui.Visible = false
+    rewardsGui.Visible = false -- Закрываем окно
     task.wait(2)
     
     sendMessage("!map Maze")
@@ -257,17 +259,19 @@ local function onGameEnd()
     print("Цикл перезапущен, ждем 3 минуты или появления окна.")
 end
 
+-- Главный цикл
 task.spawn(function()
     while _G.AutoFarmActive do
         local rewardFound = false
         local startTime = tick()
         
+        -- Цикл ожидания окна (максимум 180 секунд)
         while tick() - startTime < 180 do
             if rewardsGui.Visible == true then
                 rewardFound = true
                 break
             end
-            task.wait(1)
+            task.wait(1) -- Проверяем каждую секунду
         end
         
         if not rewardFound then
@@ -277,33 +281,20 @@ task.spawn(function()
                 character.Humanoid.Health = 0
             end
             
+            -- Ждем появления окна после ресета
             while rewardsGui.Visible == false do
                 task.wait(0.5)
             end
         end
         
+        -- Когда окно точно появилось (само или после ресета)
         onGameEnd()
     end
 end)
 
 print("Автофарм запущен: Ожидание 180с или Rewards")
-		end
-})
-
-	Tab:AddButton({
-	Name = "Stop XP FARM",
-	Callback = function()
-_G.AutoFarmActive = false
-
-if _G.RewardConnection then
-    _G.RewardConnection:Disconnect()
-    _G.RewardConnection = nil
-end
-
-print("Скрипт полностью остановлен и отключен.")
   	end    
 })
-
 
 Tab:AddButton({
 	Name = "Shutdown Game if dev join",
@@ -922,6 +913,7 @@ game.DescendantAdded:Connect(addRemote)
 print("Spy Loaded!")
   	end    
 })
+
 
 
 
