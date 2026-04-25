7767d217918e34662f350824359f1cefcfe56058
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-04-25 11:30:53 +0200

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "MeowSploit", HidePremium = false, SaveConfig = true, ConfigFolder = "🎄Evade🎄"})
+local Window = OrionLib:MakeWindow({Name = "MeowlSploit", HidePremium = false, SaveConfig = true, ConfigFolder = "EvadeMeowlSploit"})
 
 local Tab = Window:MakeTab({
 	Name = "Event farm 💌",
